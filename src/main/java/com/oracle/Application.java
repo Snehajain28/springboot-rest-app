388a7ff9ecc5d48b86811e3c8584141e080b3d6e
Snehajain28 <snehajain28022004@gmail.com>
2025-07-23 14:48:49 +0530

    msg

--- src/main/java/com/oracle/Application.java
+++ src/main/java/com/oracle/Application.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class Application {
 
 	public static void main(String[] args) {
+	System.out.println("wgusyjh");
 		SpringApplication.run(Application.class, args);
 	}
 
